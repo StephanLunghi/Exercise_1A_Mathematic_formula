ecef30912a945c73821ca00b58a60053ec555078
Stephan Lunghi <slunghi43@gmail.com>
2019-02-22 13:37:49 -0500

    --

--- Exercise 1A Mathematic formulas/Program.cs
+++ Exercise 1A Mathematic formulas/Program.cs
@@ -13,22 +13,34 @@ namespace Exercise_1A_Mathematic_formulas
         {
             
             // Basic Difficulty Questions
-            // Determining Area and Circumference from a given radius.
+            // Determining Area, Circumference, and Volume from a given radius.
             Console.WriteLine("Please enter the radius: ");
             double radius = double.Parse(Console.ReadLine());
             double area = Math.PI * (radius * radius);
             double circ = 2 * Math.PI * radius;
-            Console.WriteLine("The area is: " + area);
-            Console.WriteLine("The circumference is: " + circ);
+            double volume = 4.0 / 3 * Math.PI * (radius * radius * radius) / 2;
+            Console.WriteLine("The area is: {0:F2} ", area);
+            Console.WriteLine("The circumference is: {0:F2} ", circ);
+            Console.WriteLine("The volume is: {0:F2} ", volume);
             Console.ReadLine();
             Console.Clear();
 
             // Intermediate Difficulty Question
-            // Determining volume from a given radius length
-            // RL = Radius Length
-            Console.WriteLine("Please enter the radius length: ");
-            double RL = double.Parse(Console.ReadLine());
-            
+            // Area of a triangle given the length of sides
+            // FL = First Length   SL = Second Length   TL = Third Length   P = Half of the Circumference   A = Area
+            Console.WriteLine("Please enter the first length: ");
+            double FL = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the second length: ");
+            double SL = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the third length: ");
+            double TL = double.Parse(Console.ReadLine());
+            double P = (FL + SL + TL) / 2;
+            double A = Math.Sqrt(P * (P - FL) * (P - SL) * (P - TL));
+            Console.WriteLine("The area of the triangle is: {0:F2} ", A);
+
+            // Advanced Difficulty Question
+            //Solving a quadratic equation
+
 
         }
     }
