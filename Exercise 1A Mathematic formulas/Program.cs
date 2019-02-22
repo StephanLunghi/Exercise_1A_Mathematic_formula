c215b3961cda9a8a36100cd0a6a3e3bc533a3c59
Stephan Lunghi <slunghi43@gmail.com>
2019-02-22 17:11:22 -0500

    Trying to touch up some stuff

--- Exercise 1A Mathematic formulas/Program.cs
+++ Exercise 1A Mathematic formulas/Program.cs
@@ -38,10 +38,28 @@ namespace Exercise_1A_Mathematic_formulas
             double A = Math.Sqrt(P * (P - FL) * (P - SL) * (P - TL));
             Console.WriteLine("The area of the triangle is: {0:F2} ", A);
 
+            Console.Clear();
+
             // Advanced Difficulty Question
             // Solving a quadratic equation
-            // I had a hard time understanding how to code this, I used Stackoverflow to help with this. 
-            double 
+            // I had a hard time understanding how to code this, I used Stackoverflow and Youtube videos to help with this. I ended up using the same simple style as the other equations.
+            
+            Console.WriteLine("Input a values to solve a quadratic equation. Input a value for A");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Input a value for B");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Input a value for C");
+            double c = double.Parse(Console.ReadLine());
+
+            double Root = Math.Sqrt(b * b - 4 * a * c);
+            if (Root >= 0)
+            {
+                double x1 = (-b + Root) / 2 * a;
+                double x2 = (-b - Root) / 2 * a;
+                Console.WriteLine("x1= " + x1 + "x2= " + x2);
+            }
+            else { Console.WriteLine("Sorry, there is no root");
+                }
 
         }
     }
