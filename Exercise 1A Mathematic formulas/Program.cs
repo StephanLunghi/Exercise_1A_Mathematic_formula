9be8be1838582dd7fb9ee0f4317d6a8fedbbc126
Stephan Lunghi <slunghi43@gmail.com>
2019-02-22 12:04:26 -0500

    Struggling through exercise

--- Exercise 1A Mathematic formulas/Program.cs
+++ Exercise 1A Mathematic formulas/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ExerciseMathFunctions;
 
 namespace Exercise_1A_Mathematic_formulas
 {
@@ -10,6 +11,25 @@ namespace Exercise_1A_Mathematic_formulas
     {
         static void Main(string[] args)
         {
+            
+            // Basic Difficulty Questions
+            // Determining Area and Circumference from a given radius.
+            Console.WriteLine("Please enter the radius: ");
+            double radius = double.Parse(Console.ReadLine());
+            double area = Math.PI * (radius * radius);
+            double circ = 2 * Math.PI * radius;
+            Console.WriteLine("The area is: " + area);
+            Console.WriteLine("The circumference is: " + circ);
+            Console.ReadLine();
+            Console.Clear();
+
+            // Intermediate Difficulty Question
+            // Determining volume from a given radius length
+            // RL = Radius Length
+            Console.WriteLine("Please enter the radius length: ");
+            double RL = double.Parse(Console.ReadLine());
+            
+
         }
     }
 }
